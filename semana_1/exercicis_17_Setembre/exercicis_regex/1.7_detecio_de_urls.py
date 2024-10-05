cdb113712e701a6f11ecd5461f2727f1b3eb3fba
Levi <levifv.madrid@gmail.com>
2024-10-05 15:34:15 +0200

    changes11

--- semana_1/exercicis_17_Setembre/exercicis_regex/1.7_detecio_de_urls.py
+++ semana_1/exercicis_17_Setembre/exercicis_regex/1.7_detecio_de_urls.py
@@ -1,9 +1,6 @@
-'''
-Exercici 7: Detecció de URLs 
+''' Exercici 7: Detecció de URLs 
 Objectiu: Escriu un regex per extreure URLs d'una cadena de text. 
-Entrada: Visita https://www.example.com per més informació o http://test.org. També 
-pots anar a www.google.com
-'''
+Entrada: Visita https://www.example.com per més informació o http://test.org. També pots anar a www.google.com. '''
 
 # https:\/\/w{3}\.[a-z]+.[a-z]+|http:\/\/[a-z]+\.[a-z]+|w{3}\.[a-z]+\.[a-z]+
 
@@ -14,3 +11,12 @@ pots anar a www.google.com
 # [htpsw]+[:\/.]+[a-z]+[:\/.]+[a-z]+.?[a-z]+
 
 # (http[s]?:\/\/|(w{3})){1}[a-zA-Z\.]*[\.]?[a-z]+
+
+import re
+
+patro = re.compile(r"(?:^|\s|,)https:\/\/w{3}\.[a-z]+.[a-z]+|http:\/\/[a-z]+\.[a-z]+|w{3}\.[a-z]+\.[a-z]+(?=$|\s|,|\.)")
+
+entrada = input("Ingresa un texto que conetnga URL's: ")
+
+resultado = patro.findall(entrada)
+resultado = [print(url) for url in resultado]
--- semana_1/exercicis_17_Setembre/exercicis_regex/1.9_extraccio_de_noms_propis.py
+++ semana_1/exercicis_17_Setembre/exercicis_regex/1.9_extraccio_de_noms_propis.py
@@ -1,8 +1,12 @@
-'''
-Exercici 9: Extracció de noms propis 
-Objectiu: Escriu un regex per extreure noms propis d'una frase (inicien amb majúscula). 
-Entrada: Maria va anar a Barcelona amb Josep i Anna. 
-'''
+'''Exercici 9: Extracció de noms propis 
+Objectiu: Escriu un regex per extreure noms propis d'una frase (inicien amb ajúscula). 
+Entrada: Maria va anar a Barcelona amb Josep i Anna. '''
 
-# tentando
-# [A-Z]{1}[^\s()\/\\"@0-9:]+
+import re
+
+patro = re.compile(r"(?:^|\s|,)[A-ZÁÉÍÓÚÀÈÒÑ][a-záéíóúàèòñ]*(?=$|\s|,|\.)")
+
+entrada = input("Ingresa un texto que contenga palabras que inicien con mayusculas: ")
+
+resultado = patro.findall(entrada)
+resultado = [print(nou.strip(' ')) for nou in resultado]
