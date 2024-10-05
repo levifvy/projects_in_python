1a2e59bb9463decfe9fd5b5d4005fa6bc7ad55a0
Levi <levifv.madrid@gmail.com>
2024-10-05 10:53:47 +0200

    changes

--- semana_1/exercicis_17_Setembre/exercicis_regex/1.2_extraccio_de_numeros_de_telefon_amb_prefix.py
+++ semana_1/exercicis_17_Setembre/exercicis_regex/1.2_extraccio_de_numeros_de_telefon_amb_prefix.py
@@ -1,16 +1,16 @@
+'''Exercici 2: Extracció de números de telèfon amb prefix
+Objectiu: Escriu una expressió regular per extreure els números de telèfon espanyols amb prefix "(93)".
+Entrada: (93)2541700, (952)253228, (93)2177017, (958)233517, (93), (93)4561233.
 '''
-xercici 2: Extracció de números de telèfon amb prefix
-Objectiu: Escriu una expressió regular per extreure els números de telèfon espanyols amb 
-prefix "(93)".
-Entrada: (93)2541700, (952)253228, (93)2177017, (958)233517.
-'''
+import re
 
+#patro = re.compile("?<=^|\s|,)\([0-9]+\)[0-9]{5,}(?=$|\s|,|\.")
+patro = re.compile(r"(?:^|\s|,)\(93\)[0-9]{5,}(?=$|\s|,|\.)")
 
-# (?<=^|\s|,)\([0-9]+\)[0-9]{5,}(?=$|\s|,|\.)
+entrada = input("Ingrese un texto con numeros telefonicos: ")
 
-# Exercici 2: Extracció de números de telèfon amb prefix
-# Objectiu: Escriu una expressió regular per extreure els números de telèfon espanyols amb 
-# prefix "(93)".
-# Entrada: (93)2541700, (952)253228, (93)2177017, (958)233517.
-# (93)2541700
-# (93)
+resultado = patro.findall(entrada)
+resultado = [telefono.strip(", ") for telefono in resultado]
+
+for telefono in resultado:
+    print(telefono)
