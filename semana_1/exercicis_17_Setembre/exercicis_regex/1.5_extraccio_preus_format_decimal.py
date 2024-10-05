32b73352171d81498fb9b68b7eb6fa891a27ab12
Levi <levifv.madrid@gmail.com>
2024-10-05 14:11:51 +0200

    changes

--- semana_1/exercicis_17_Setembre/exercicis_regex/1.5_extraccio_preus_format_decimal.py
+++ semana_1/exercicis_17_Setembre/exercicis_regex/1.5_extraccio_preus_format_decimal.py
@@ -1,12 +1,12 @@
-'''
-Exercici 5: Extracció de preus en format decimal
+'''Exercici 5: Extracció de preus en format decimal
 Objectiu: Escriu un regex per trobar preus amb decimals en una cadena de text.
-Entrada: Els preus dels productes són 10.99€, 20€, 100.50€, i 300.00€
+Entrada: Els preus dels productes són 10.99€, 20€, 100.50€, i 300.00€, 50,25€
 '''
+import re
 
-# [0-9]*\.[0-9]*€
+patro = re.compile(r"(?:^|\s|,)[0-9]*[\.,]{1}[0-9]*€(?=$|\s|,|\.)")
 
+entrada = input("Ingresa un texto con precios de productos: ")
 
-# [0-9]*[\.,]{1}[0-9]*€
-
-# \s[0-9]*[\.,]{1}[0-9]*€
+resultado = patro.findall(entrada)
+resultado = [print(preu.strip(" ")) for preu in resultado]
--- semana_1/exercicis_17_Setembre/exercicis_regex/1.6_validacio_dades_format_fecha.py
+++ semana_1/exercicis_17_Setembre/exercicis_regex/1.6_validacio_dades_format_fecha.py
@@ -1,5 +1,33 @@
-'''
-Exercici 6: Validació de dates en format DD/MM/AAAA 
+'''Exercici 6: Validació de dates en format DD/MM/AAAA 
 Objectiu: Escriu un regex per validar dates en format DD/MM/AAAA. 
-Entrada: 25/12/2021, 01/01/1999, 31/02/2020, 30/04/2020.
+Entrada: 25/12/2021, 01/01/1999, 31/02/2020, 30/04/2020, 31/02/2024, 54/20/2024
 '''
+import re
+
+#patro = re.compile(r"(?:^|\s|,)[0-9]{2}\/[0-9]{2}\/[0-9]{4}(?=$|\s|,|\.)")
+patro = re.compile(r"(?:^|\s|,)(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/([0-9]{4})(?=$|\s|,|\.)")
+
+entrada = input("Ingresa un texto con fechas: ")
+
+resultado = patro.findall(entrada)
+
+def es_anio_bisiesto(anio):
+    return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
+
+def es_fecha_valida(dia, mes, anio):
+    dia = int(dia)
+    mes = int(mes)
+    anio = int(anio)
+
+    if mes < 1 or mes > 12:
+        return False
+
+    # ternaria
+    dias_por_mes = [31, 29 if es_anio_bisiesto(anio) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    if dia < 1 or dia > dias_por_mes[mes - 1]:
+        return False
+
+    return True
+
+fechas_validas = [print('/'.join(fecha)) for fecha in resultado if es_fecha_valida(*fecha)]
